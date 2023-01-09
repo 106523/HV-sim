fb88082137471a7004bb76f83bee115d0b4a269d
106523 <102621831+106523@users.noreply.github.com>
2023-01-09 11:46:18 -0600

    Optimize steptimer

--- JS/main.js
+++ JS/main.js
@@ -4,10 +4,8 @@ var StepLastTime
 
 setInterval(function(){RegularLoop(); }, 1);
 function RegularLoop() {
-  const timestamp = (Date.now());
+// const timestamp = Date.now();
   console.log(StepTime)
-  console.log(timestamp)
-  console.log(lastTimestamp)
-  StepTime = timestamp-lastTimestamp
-  lastTimestamp = timestamp;
+  StepTime = Date.now()-lastTimestamp
+  lastTimestamp = Date.now();
 }
