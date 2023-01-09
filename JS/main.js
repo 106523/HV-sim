c9d3a8cfd03d2532b84a32477dc79124f7aecd3d
106523 <102621831+106523@users.noreply.github.com>
2023-01-09 09:44:55 -0600

    get steptime implemented

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,15 @@
+var StepTime
+var StepLastTime = getTime();
+
+
+
+
 setInterval(function(){RegularLoop(); }, 1);
 
 function RegularLoop() {
+  //Calculate the step time to compensate for lag/slowdowns
+  StepTime = getTime() - StepLastTime;
+  StepLastTime = getTime();
+  
+  
 }
