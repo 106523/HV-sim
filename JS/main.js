66ce6fe4f2e002645302daadd1e1ad6782caa977
106523 <102621831+106523@users.noreply.github.com>
2023-01-10 09:17:30 -0600

    Improved doccumentation

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,8 @@ var StepLastTime
 
 setInterval(function(){RegularLoop(); }, 1);
 function RegularLoop() {
-// const timestamp = Date.now();
+  // const timestamp = Date.now();
+  //checks how long each step is so the program can compensate for execution speeds
   console.log(StepTime)
   StepTime = Date.now()-lastTimestamp
   lastTimestamp = Date.now();
