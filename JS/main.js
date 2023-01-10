b506a660a2ec36b046c01899006c99e37a95e187
106523 <102621831+106523@users.noreply.github.com>
2023-01-10 09:45:34 -0600

    Getting some variables in, changing function name.

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,17 @@
+//variables the main function will be dealing (eg. speed) with NEED to be global. 
+
+//powertrain variables
+var speed = 0;
+//Resistance variables
+var BaseResistance = 200;
+var ResistanceExponental = 1;
+//Timer variables
 let lastTimestamp = 0; 
 var StepTime 
 var StepLastTime 
 
-setInterval(function(){RegularLoop(); }, 1);
-function RegularLoop() {
+setInterval(function(){Main(); }, 1);
+function Main() {
   // const timestamp = Date.now();
   //checks how long each step is so the program can compensate for execution speeds
   console.log(StepTime)
