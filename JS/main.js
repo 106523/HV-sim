aad77cdcc4e773fa8238c5b6496a362847138680
106523 <102621831+106523@users.noreply.github.com>
2023-01-18 10:45:44 -0600

    not much

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 //variables the main function will be dealing (eg. speed) with NEED to be global. 
 //Perhaps I will optimise the debug ui text thing, make it NON global, just easier to use a global for now.
+//might be a good idea to just send the vars spitted out by the torque calculate module to bypass having some vars just floating around in the HV/EV controller. 
 var DebugUItext
 //powertrain variables
 var speed = 0;
@@ -50,6 +51,7 @@ function EVMode(TorqueDemand, MG1TorqueLimit) {
   let EngineTorqueOutput = 0;
   let EngineGeneration = 0;
   let EngineRPM = 0;
+  //why the FUCK did I make MG1Torquelimit an absolute? Did it ever go into the NEGATIVE range for god knows what reason?!
   if (TorqueDemand >= Math.abs(MG1TorqueLimit)) {
       MG1TorqueOutput = MG1TorqueLimit;
     } else {
